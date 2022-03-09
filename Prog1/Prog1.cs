6670acc04ad0146a4a7881cf647e3655bd6b522e
monlyaha <igorahtulov@gmail.com>
2022-03-09 11:24:56 +0300

    Добавьте файлы проекта.

---
+++ Prog1/Prog1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog1
+{
+    internal class Prog1
+    {
+        /// <summary>
+        /// Получение готовой строки
+        /// </summary>
+        /// <param name="number">Максимальное значение для строки</param>
+        /// <returns>Готовая строка</returns>
+        static string getOrder(int number) 
+        {
+            string result = "";
+
+            for (int i = 1; i <= number; i++)           
+            {
+                result = result + i + " ";
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Запрос на ввод числа и вывод результата
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string result;
+            Console.Write("Введите положительное число: ");
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Число должно превышать 0!");
+                }
+                else
+                {
+                    result = getOrder(number);
+                    Console.WriteLine(result);
+                }
+            }
+            catch
+            { 
+                Console.WriteLine("Введено неверное значение!");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Prog2/Prog2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog2
+{
+    internal class Prog2
+    {
+        /// <summary>
+        /// Вычисление центра квадрата и его отрисовка
+        /// </summary>
+        /// <param name="length">Длина стороны квадрата</param>
+        /// <returns>Квадрат с отверстием в центре в виде строки</returns>
+        static string getOrder(double length)
+        {
+            string result = "";
+            if (length == 1)
+            {
+                result += "*";
+            }
+            else
+            {
+                if (length % 2 == 0)
+                {
+                    Console.WriteLine("Для получения отверстия сторона квадрата должна быть нечетной длины!");
+                }
+                else
+                {
+                    double middlePoint = Convert.ToInt32((length / 2) - 0.5);
+
+                    for (int i = 0; i < length; i++)
+                    {
+                        for (int j = 0; j <= length; j++)
+                        {
+                            if (i != middlePoint)
+                            {
+                                if (j != length)
+                                {
+                                    result += "* ";
+                                }
+                                else
+                                {
+                                    result += "\n";
+                                }
+                            }
+                            else
+                            {
+                                if (j != length)
+                                {
+                                    if (j != middlePoint)
+                                    {
+                                        result += "* ";
+                                    }
+                                    else
+                                        result += "  ";
+                                }
+                                else
+                                {
+                                    result += "\n";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Главная функция, запрашивает длину квадрата и выводит готовый результат
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string result;
+            Console.Write("Введите длину стороны квадрата: ");
+
+            try
+            {
+                double length = Convert.ToInt32(Console.ReadLine());
+
+                if (length < 0)
+                {
+                    Console.WriteLine("Число должно превышать 0!");
+                }
+                else
+                {
+                    result = getOrder(length);
+                    Console.WriteLine(result);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Введено неверное значение!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
